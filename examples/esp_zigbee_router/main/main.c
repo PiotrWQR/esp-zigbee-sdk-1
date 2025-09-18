173a119056a86dcd8c4dc3355b48459a65d0ac06
PiotrWQR <6188@akademiazamojska.edu.pl>
2025-09-18 23:43:00 +0200

    buffer size

--- examples/esp_zigbee_router/main/main.c
+++ examples/esp_zigbee_router/main/main.c
@@ -158,7 +158,7 @@ static esp_err_t zb_register_device(void){
 
 static void esp_zb_task(void *pcParameters)
 {
-
+    ESP_ERROR_CHECK(esp_zb_io_buffer_size_set(200));
     esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZR_CONFIG();
     esp_zb_init(&zb_nwk_cfg);
 
