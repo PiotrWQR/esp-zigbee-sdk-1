9c48c6e67703730123cf1c2ffba3372558373e8c
PiotrWQR <6188@akademiazamojska.edu.pl>
2025-10-06 23:50:42 +0200

    poprawy

--- examples/esp_zigbee_cordinator/components/Helpers/include/Helpers.h
+++ examples/esp_zigbee_cordinator/components/Helpers/include/Helpers.h
@@ -27,13 +27,15 @@ void refresh_routes(void);
 void traffic_reporter_init(void *pvParameters);
 void zero_traffic_raport(void);
 void send_settings(uint16_t short_addr);
-
+//tablice pomocnicze
+//tablica dopasowania typu urządzenia do nazwy enumeratora
 static const char *dev_type_name[] = {
     [ESP_ZB_DEVICE_TYPE_COORDINATOR] = "ZC",
     [ESP_ZB_DEVICE_TYPE_ROUTER]      = "ZR",
     [ESP_ZB_DEVICE_TYPE_ED]          = "ZED",
     [ESP_ZB_DEVICE_TYPE_NONE]        = "UNK",
 };
+//tablica nazw relacji z innymi urządzeniami
 static const char rel_name[] = {
     [ESP_ZB_NWK_RELATIONSHIP_PARENT]                = 'P', /* Parent */
     [ESP_ZB_NWK_RELATIONSHIP_CHILD]                 = 'C', /* Child */
@@ -42,12 +44,14 @@ static const char rel_name[] = {
     [ESP_ZB_NWK_RELATIONSHIP_PREVIOUS_CHILD]        = 'c', /* Previous Child */
     [ESP_ZB_NWK_RELATIONSHIP_UNAUTHENTICATED_CHILD] = 'u', /* Unauthenticated Child */
 };
+//tablica nazw stanów trasy
 static const char *route_state_name[] = {
     [ESP_ZB_NWK_ROUTE_STATE_ACTIVE] = "Active",
     [ESP_ZB_NWK_ROUTE_STATE_DISCOVERY_UNDERWAY] = "Disc",
     [ESP_ZB_NWK_ROUTE_STATE_DISCOVERY_FAILED] = "Fail",
     [ESP_ZB_NWK_ROUTE_STATE_INACTIVE] = "Inactive",
 };
+//tablica rozmiarów nagłówków w zależności od trybu adresowania(niedopracowana)
 static const uint8_t aps_address_modes_size[] = {
     [ESP_ZB_APS_ADDR_MODE_DST_ADDR_ENDP_NOT_PRESENT]   = 0,
     [ESP_ZB_APS_ADDR_MODE_16_GROUP_ENDP_NOT_PRESENT]   = 2,
@@ -55,6 +59,7 @@ static const uint8_t aps_address_modes_size[] = {
     [ESP_ZB_APS_ADDR_MODE_64_ENDP_PRESENT]             = 49,
     [ESP_ZB_APS_ADDR_MODE_64_PRESENT_ENDP_NOT_PRESENT] = 8,
 };
+
 typedef struct esp_zb_network_traffic_raport_s {
     uint16_t short_addr;      //Short address of the reporting device
     bool is_active;
@@ -65,6 +70,7 @@ typedef struct esp_zb_network_traffic_raport_s {
     uint32_t missed_packets;
 } esp_zb_network_traffic_raport_t;
 
+//informacje od urządzenia końcowego  o statystykach zebranych z zakończonej transmisji
 typedef struct data_recived_s {
     uint32_t start_time;
     uint32_t end_time;
@@ -72,22 +78,24 @@ typedef struct data_recived_s {
     uint32_t successful_ping_count;
 } data_recived_t;
 
+//ładunek pakietu ping
 typedef struct ping_payload_s {
-    uint32_t seq_num;
-    uint32_t send_time;
-    uint32_t max_ping_count;
-    uint8_t *payload;
+    uint32_t seq_num; //numer nadany indykatorowi przez nadawcę
+    uint32_t send_time; // czas wysłania indykatora z nadawcy
+    uint32_t max_ping_count; // maksymalna liczba indykatorów do wysłania
+    uint8_t *payload; //dodatkowy ładunek(zwykle losowe/ustawione bajty)
 } ping_payload_t;
 
 typedef struct {
     uint16_t new_repeats;
     uint16_t new_dest_addr;
-    uint32_t new_delay_ms;
-    uint32_t new_delay_tick;
+    uint32_t new_delay_ms;      // przerwa miedzy żądaniami dla wysyłania sekwancji pingow przez urządzenie końcowe
+    uint32_t new_delay_tick; // przerwa miedzy żądaniami dla wysyłania sekwancji pingow przez urządzenie końcowe(nieuzywane)
     uint8_t csma_min_be;        /*!< The minimum value of the backoff exponent, BE, in the CSMA-CA algorithm. */
     uint8_t csma_max_be;        /*!< The maximum value of the backoff exponent, BE, in the CSMA-CA algorithm. */
-    uint8_t csma_max_backoffs;
+    uint8_t csma_max_backoffs;  /*!< The maximum number of backoff attempts, NB, in the CSMA-CA algorithm. */
 } setting_change_t;
+
 
 typedef struct neighbor_info_s{
     uint16_t short_addr;
@@ -105,6 +113,7 @@ typedef struct route_info_s {
     uint16_t next_hop;
     uint8_t flags;
 } route_info_t;
+// struktura zawierająva dane o sąsiadach i trasach zebrane z urządzenia nadjacego(zazwyczaj routera)
 typedef struct topology_report_s {
     esp_zb_ieee_addr_t ieee_addr;
     int16_t neighbor_count;
